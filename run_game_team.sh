74f1be2442cb56b01069d841861fb937e4a2fb2a
Michael Bloom <MikeBloom914@gmail.com>
2018-06-07 16:48:09 -0400

    Team Game

---
+++ run_game_team.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#./halite -d "240 160" "python3 MyBot2v2.py" "python3 MyBot.py" "python3 MyBot-jtv5.py" "python3 MyBot4f.py"
+./halite -d "240 160" "python3 MyBot-v5.py" "python3 MyBot-team.py" "python3 MyBot-sent.py" "python3 MyBot-jtv4.py" --team
